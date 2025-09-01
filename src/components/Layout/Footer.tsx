f11b33ce819524d226c3e80e776b5098f52ae036
wei <daviddiligent@gmail.com>
2025-09-01 11:23:08 +0800

    added github link

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -6,9 +6,8 @@ export const Footer: React.FC = () => {
       <div className="container mx-auto px-4 text-center">
         <p>© 2023 DrBEE. All rights reserved.</p>
         <div className="mt-4 flex justify-center space-x-4">
-          <a href="#" className="hover:text-yellow-400">Terms</a>
-          <a href="#" className="hover:text-yellow-400">Privacy</a>
           <a href="/about" className="hover:text-yellow-400">About</a>
+          <a href="https://github.com/wildcoloratcn/drbeeweb3" className="hover:text-yellow-400" target="_blank" rel="noopener noreferrer">Github</a>
         </div>
       </div>
     </footer>
